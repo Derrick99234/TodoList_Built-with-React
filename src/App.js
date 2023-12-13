f74d841adf7844f5a0c6c62d2ca05fd0652a271a
Derrick99234 <pshubomi@gmail.com>
2023-12-13 23:35:35 +0100

    Done with Todo App -- Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+// import GenerateP from "./components/PasswordGenerator/GenerateP";
+
+// import BgChange from "./components/bg-changer/BgChange";
+import Calculator from "./components/Calculator App/Calculator";
+// import Meal from "./components/Meal App/Meal";
+// import MealApp from "./components/Meal App By Derrick/MealApp";
+// import useFetch from "./components/useFetch";
+// import { useEffect, useState } from "react";
+// import Todo from "./components/Todo App/todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Meal />
+      <MealApp /> 
+      <BgChange />
+      <GenerateP />
+      <Todo/> 
+      */}
+      <Calculator></Calculator>
+    </>
   );
 }
 
---
+++ src/components/todoTrial/trial.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./style.css";
+
+const generator = () => {
+  return Math.floor(Math.random() * 100);
+};
+
+const Trail = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const deleteTodo = (id) => {
+    setTodos((todos) => todos.filter((item) => item.id !== id));
+  };
+
+  const addTodo = () => {
+    setTodos((todos) =>
+      todos.concat({
+        text: input,
+        id: generator(),
+      })
+    );
+    setInput("");
+
+    console.log(todos);
+    // setInput("");
+  };
+
+  return (
+    <>
+      <div className="container">
+        <input
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+        <div className="todo-list">
+          {todos.map(({ text, id }) => (
+            <p key={id} className="todo">
+              {text}{" "}
+              <button onClick={() => deleteTodo(id)} className="close">
+                X
+              </button>
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Trail;
